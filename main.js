aeefc9ba41d6c1bb223cee0cca3e5b93ec7fbdac
danichimescu <constantindan@gmail.com>
2025-10-11 01:50:20 +0300

    update landing page with an animation

---
+++ main.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', () => {
+    const casaOverlay = document.getElementById('casaOverlay');
+    setTimeout(() => {
+        casaOverlay.classList.add('animate');
+    }, 1500);
+});
